5426e1b0d8075d1394ae46647734f640a8a9f181
meyer30 <meyer30@illinois.edu>
2014-03-07 04:41:12 -0600

    meyer30: Added Test Cases for NewWorkoutActivity

---
+++ WorkoutBuddyTest/src/com/example/workoutbuddy/test/NewWorkoutActivityTest.java
@@ -0,0 +1,66 @@
+package com.example.workoutbuddy.test;
+
+import junit.framework.Assert;
+import android.annotation.SuppressLint;
+import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.workoutbuddy.NewWorkoutActivity;
+import com.example.workoutbuddy.R;
+
+public class NewWorkoutActivityTest extends
+	ActivityInstrumentationTestCase2<NewWorkoutActivity> {
+
+	private NewWorkoutActivity nwActivity;
+
+	
+	public NewWorkoutActivityTest(){
+		super(NewWorkoutActivity.class);
+	}
+
+	@Override
+	  protected void setUp() throws Exception {
+	    super.setUp();
+
+	    setActivityInitialTouchMode(false);
+	    
+	    nwActivity = getActivity();
+	}
+
+	public void testPreConditions(){
+		Assert.assertEquals(1, nwActivity.numExercises);
+	}
+	
+	
+	public void testLayout(){
+		testButtonLayout(nwActivity.findViewById(R.id.buttonMinus));
+		testButtonLayout(nwActivity.findViewById(R.id.buttonPlus));
+		testButtonLayout(nwActivity.findViewById(R.id.buttonDone));
+	}
+	
+	public void testButtonLayout(View view)
+	{
+		Assert.assertNotNull(view);
+		Assert.assertTrue(view.isShown());
+		Assert.assertTrue(view.isClickable());
+	}
+	
+	@SuppressLint("NewApi")
+	public void testbuttonDoneClick(){
+		Button button = (Button)nwActivity.findViewById(R.id.buttonDone);
+		Assert.assertTrue(button.callOnClick());
+	}
+	
+	@SuppressLint("NewApi")
+	public void testbuttonPlusClick(){
+		Button button = (Button)nwActivity.findViewById(R.id.buttonPlus);
+		Assert.assertTrue(button.callOnClick());
+	}
+	
+	@SuppressLint("NewApi")
+	public void testbuttonMinusClick(){
+		Button button = (Button)nwActivity.findViewById(R.id.buttonMinus);
+		Assert.assertTrue(button.callOnClick());
+	}
+}
