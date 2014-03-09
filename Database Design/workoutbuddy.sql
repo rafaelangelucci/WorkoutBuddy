b6441922c70fa53b8b3f95c6c883c11c27c6386f
Weixi Liu <mxliuweixi@hotmail.com>
2014-03-08 21:07:48 -0600

    Add User to Database
    1. User added to DB
    2. add priority to template exercise and set
    3. update ER Diagram

--- Database Design/workoutbuddy.sql
+++ Database Design/workoutbuddy.sql
@@ -1,22 +1,30 @@
+CREATE TABLE User(
+       username char(50) PRIMARY KEY,
+       password char(50)
+);
+
 CREATE TABLE Exercise(
        e_id  INT PRIMARY KEY,
        type char(10),
        name char(50),
-       description char(200)
+       description char(200),
+       username char(50) REFERENCES User(username)
 );
 
 CREATE TABLE Workout(
        w_id INT PRIMARY KEY,
        name char(50),
        date DATE UNIQUE,
-       description char(200)
+       description char(200),
+       username char(50) REFERENCES User(username)
 );
 
 CREATE TABLE Sets(
        s_id INT PRIMARY KEY,
        reps INT,
        weight INT,
-       time INTERVAL HOUR(8) TO MINUTE,
+       time TIME,
+       priority INT,
        e_id INT REFERENCES Exercise(e_id),
        w_id INT REFERENCES Workout(w_id)	       
 );
@@ -32,14 +40,15 @@ CREATE TABLE Template_Workout(
        t_id INT PRIMARY KEY,
        name char(50),
        description char(200)
+       username char(50) REFERENCES User(username)
 );
 
 CREATE TABLE Template_Linker(
        te_id INT REFERENCES Template_Exercise(te_id),
        t_id INT REFERENCES Template_Workout(t_id),
+       priority INT,
        primary key (te_id, t_id)
 );
-
 
 
 
