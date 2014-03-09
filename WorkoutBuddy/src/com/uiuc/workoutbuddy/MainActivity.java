35d54b0a23526220702d3491978bfc6a57f9cae4
tmadigan7 <tmadigan7@hotmail.com>
2014-03-09 12:57:47 -0500

    3,madigan6: set onClick for my workouts to pull from db - works successfully and logs results

--- WorkoutBuddy/src/com/uiuc/workoutbuddy/MainActivity.java
+++ WorkoutBuddy/src/com/uiuc/workoutbuddy/MainActivity.java
@@ -56,8 +56,7 @@ public class MainActivity extends FragmentActivity implements
 		// When swiping between different sections, select the corresponding
 		// tab. We can also use ActionBar.Tab#select() to do this if we have
 		// a reference to the Tab.
-		mViewPager
-				.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+		mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
 					@Override
 					public void onPageSelected(int position) {
 						actionBar.setSelectedNavigationItem(position);
