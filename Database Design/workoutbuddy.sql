74589f8892098102e6c7b81e32b08cd1466a8b93
Weixi Liu <mxliuweixi@hotmail.com>
2014-03-08 22:39:35 -0600

    Update constraints in relations

--- Database Design/workoutbuddy.sql
+++ Database Design/workoutbuddy.sql
@@ -4,50 +4,58 @@ CREATE TABLE User(
 );
 
 CREATE TABLE Exercise(
-       e_id  INT PRIMARY KEY,
+       e_id  INT PRIMARY KEY AUTO_INCREMENT,
        type char(10),
        name char(50),
        description char(200),
-       username char(50) REFERENCES User(username)
+       username char(50),
+       FOREIGN KEY(username) REFERENCES User(username)
 );
 
 CREATE TABLE Workout(
-       w_id INT PRIMARY KEY,
+       w_id INT PRIMARY KEY AUTO_INCREMENT,
        name char(50),
        date DATE UNIQUE,
        description char(200),
-       username char(50) REFERENCES User(username)
+       username char(50),
+       FOREIGN KEY(username) REFERENCES User(username)
 );
 
 CREATE TABLE Sets(
-       s_id INT PRIMARY KEY,
+       s_id INT PRIMARY KEY AUTO_INCREMENT,
        reps INT,
        weight INT,
        time TIME,
        priority INT,
-       e_id INT REFERENCES Exercise(e_id),
-       w_id INT REFERENCES Workout(w_id)	       
+       e_id INT,
+       w_id INT,
+       FOREIGN KEY(e_id) REFERENCES Exercise(e_id),
+       FOREIGN KEY(w_id) REFERENCES Workout(w_id)
 );
 
 CREATE TABLE Template_Exercise(
-       te_id INT PRIMARY KEY,
-       e_id INT REFERENCES Exercise(e_id),
+       te_id INT PRIMARY KEY AUTO_INCREMENT,
+       e_id INT,
        numSets INT,
-       reps INT
+       reps INT,
+       FOREIGN KEY(e_id) REFERENCES Exercise(e_id)
 );
 
 CREATE TABLE Template_Workout(
-       t_id INT PRIMARY KEY,
+       t_id INT PRIMARY KEY AUTO_INCREMENT,
        name char(50),
-       description char(200)
-       username char(50) REFERENCES User(username)
+       description char(200),
+       username char(50),
+       FOREIGN KEY(username) REFERENCES User(username)
 );
 
 CREATE TABLE Template_Linker(
-       te_id INT REFERENCES Template_Exercise(te_id),
-       t_id INT REFERENCES Template_Workout(t_id),
+       te_id INT,
+       t_id INT,
        priority INT,
-       primary key (te_id, t_id)
+       primary key (te_id, t_id),
+       FOREIGN KEY(t_id) REFERENCES Template_Workout(t_id),
+       FOREIGN KEY(te_id) REFERENCES Template_Exercise(te_id)
 );
 
 
