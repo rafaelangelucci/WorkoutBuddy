39ae36735c905bd9fb9aa360bfafd0dd3bae154a
aicamargo <aicnov@gmail.com>
2014-03-16 12:37:17 -0500

    4, camargo3, added timer activity

---
+++ WorkoutBuddy/src/com/uiuc/workoutbuddy/TimerActivity.java
@@ -0,0 +1,39 @@
+package com.uiuc.workoutbuddy;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class TimerActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_timer);
+	}
+
+	public void startAlert(View view) {
+		EditText text = (EditText) findViewById(R.id.time);
+		
+		int i = Integer.parseInt(text.getText().toString());
+		
+		Intent intent = new Intent(this, TimerReceiver.class);
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(
+				this.getApplicationContext(), 234324243, intent, 0);
+		
+		AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+		alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()
+				+ (i * 1000), pendingIntent);
+		
+		// Small pop up
+		Toast.makeText(this, "Resting for " + i + " seconds",
+				Toast.LENGTH_LONG).show();
+	}
+
+}
---
+++ WorkoutBuddy/src/com/uiuc/workoutbuddy/TimerReceiver.java
@@ -0,0 +1,22 @@
+package com.uiuc.workoutbuddy;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Vibrator;
+import android.widget.Toast;
+
+public class TimerReceiver extends BroadcastReceiver {
+	
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// Small pop up
+		Toast.makeText(context, "Rest Period Over", Toast.LENGTH_LONG).show();
+		
+		// Vibrate the phone
+		Vibrator vibrator = (Vibrator) context
+				.getSystemService(Context.VIBRATOR_SERVICE);
+		vibrator.vibrate(2000);
+	}
+
+}
