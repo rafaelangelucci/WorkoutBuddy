757a3a05e6d27f3174f0849dcd30f7817ce37e72
aicamargo <aicnov@gmail.com>
2014-03-19 20:01:12 -0500

    4, camargo3, Signup button adds user to db

--- WorkoutBuddy/src/com/uiuc/workoutbuddy/LoginActivity.java
+++ WorkoutBuddy/src/com/uiuc/workoutbuddy/LoginActivity.java
@@ -1,14 +1,28 @@
 package com.uiuc.workoutbuddy;
+import httpRequests.AsyncHttpPostWrapper;
+import httpRequests.HttpRequestListener;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-public class LoginActivity extends Activity implements OnClickListener{
+public class LoginActivity extends Activity implements OnClickListener, HttpRequestListener
+{
+	CountDownLatch signal;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +47,7 @@ public class LoginActivity extends Activity implements OnClickListener{
 			Toast.makeText(this.getApplicationContext(), "Login Clicked", Toast.LENGTH_SHORT).show();
 			break;
 		case R.id.signup_button:
-			Toast.makeText(this.getApplicationContext(), "Sign Up Clicked", Toast.LENGTH_SHORT).show();
+			signup(v);
 			break;
 		case R.id.skip_button:
 			Intent intent = new Intent(this, MainActivity.class);
@@ -41,6 +55,45 @@ public class LoginActivity extends Activity implements OnClickListener{
 			break;
 		default:
 		}
+	}
+	
+	private void signup(View v)
+	{
+		// Get user password and username
+		EditText et = (EditText)findViewById(R.id.inputUsername);
+		String username = et.getText().toString();
+		
+		et = (EditText)findViewById(R.id.inputPassword);
+		String password = et.getText().toString();
+	
+	
+		if(username.equals(""))
+			Toast.makeText(this.getApplicationContext(), "The name textbox must be filled.", Toast.LENGTH_SHORT).show();
+		else
+		{
+			AsyncHttpPostWrapper wrapper = new AsyncHttpPostWrapper(this);
+			signal = new CountDownLatch(1);
+			try {
+				wrapper.addUser(username, password);
+				signal.await(2, TimeUnit.SECONDS);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				e.printStackTrace();
+			}
+			
+			Toast.makeText(this.getApplicationContext(), "Registration Complete", Toast.LENGTH_SHORT).show();
+			
+			Intent intent = new Intent(this, MainActivity.class);
+	    	startActivity(intent);
+		}
+	}
+
+	@Override
+	public void requestComplete() {
+		Log.i( "requestComplete()", "Request Completed countDown()");
+		signal.countDown();
+		
 	}
 
 }
