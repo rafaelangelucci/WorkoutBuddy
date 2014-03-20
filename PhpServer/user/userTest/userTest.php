8966d1b9da714062d8d22bf730362ed95b720711
Weixi Liu <mxliuweixi@hotmail.com>
2014-03-19 21:36:23 -0500

    Add php test to User Login User Story

--- PhpServer/user/userTest/userTest.php
+++ PhpServer/user/userTest/userTest.php
@@ -4,7 +4,7 @@ require_once '../../helperFunctions.php';
 
 class MySQLConnectTest extends PHPUnit_Framework_TestCase
 {
-    public function testUser(){
+    public function testValidUser(){
         //Curl for adding User
         $url = 'http://workoutbuddy.web.engr.illinois.edu/PhpFiles/addUser.php';
         $data = array(
@@ -15,13 +15,12 @@ class MySQLConnectTest extends PHPUnit_Framework_TestCase
         //Curl for login user
         $getUrl = 'http://workoutbuddy.web.engr.illinois.edu/PhpFiles/userLogin.php';
         $getData = array(
-            'username' => 'usernameA',
-            'password' => 'passwordA');
+            'username' => 'usernameA');
         $getResponse = curlHelper($getUrl, $getData);
         $responseArray = json_decode($getResponse);
 
         $this->assertEquals($responseArray[0]->{'username'}, 'usernameA');
-        //$this->assertEquals($responseArray[0]->{'password'}, 'passwordA');
+        $this->assertEquals($responseArray[0]->{'password'}, 'passwordA');
 
         //Curl for deleting user
         $deleteUrl = 'http://workoutbuddy.web.engr.illinois.edu/PhpFiles/deleteUser.php';
@@ -29,8 +28,31 @@ class MySQLConnectTest extends PHPUnit_Framework_TestCase
             'username' => 'usernameA',
             'password' => 'passwordA');
         $deleteResponse = curlHelper($deleteUrl, $deleteData);
+    }
 
+    public function testInvalidUser(){
+        //Curl for adding User
+        $url = 'http://workoutbuddy.web.engr.illinois.edu/PhpFiles/addUser.php';
+        $data = array(
+            'username' => 'usernameA',
+            'password' => 'passwordA');
+        $response = curlHelper($url, $data);
 
+        //Curl for login user
+        $getUrl = 'http://workoutbuddy.web.engr.illinois.edu/PhpFiles/userLogin.php';
+        $getData = array(
+            'username' => 'username');
+        $getResponse = curlHelper($getUrl, $getData);
+        $responseArray = json_decode($getResponse);
+
+        $this->assertEquals($responseArray, 'fail');
+
+        //Curl for deleting user
+        $deleteUrl = 'http://workoutbuddy.web.engr.illinois.edu/PhpFiles/deleteUser.php';
+        $deleteData = array(
+            'username' => 'usernameA',
+            'password' => 'passwordA');
+        $deleteResponse = curlHelper($deleteUrl, $deleteData);
     }
 
 
