e11e3d7c9ac631ebb15c1185c786f7b2031cd63b
meyer30 <meyer30@illinois.edu>
2014-04-04 03:04:41 -0500

    5.meyer30 Added functionality to change schedule

--- WorkoutBuddy/src/com/uiuc/workoutbuddy/Schedule.java
+++ WorkoutBuddy/src/com/uiuc/workoutbuddy/Schedule.java
@@ -1,6 +1,7 @@
 package com.uiuc.workoutbuddy;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.concurrent.ExecutionException;
@@ -15,13 +16,57 @@ import httpRequests.HttpRequestListener;
  */
 public class Schedule implements HttpRequestListener{	
 	
+
+
+	public void repeatWorkout(Workout w1, String dateStr) {
+		if(isDateStrValid(dateStr))
+		{
+			Workout w2 = new Workout(w1.getName(), dateStr, w1.getDescription(), w1.getUsername(), w1.getExercises());
+			
+
+			AsyncHttpPostWrapper wrapper = new AsyncHttpPostWrapper(this);
+			try {
+				wrapper.addWorkout(w2);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 	
-	
+
+
+	public void repeatWorkout(Workout w, String startDateStr, int numWeeks, boolean everyWeek) {
+		if(isDateStrValid(startDateStr))
+		{
+			DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+			Calendar cal = Calendar.getInstance();
+			try {
+				cal.setTime(df.parse(startDateStr));
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return;
+			}
+			
+			
+			for(int idx = 0; idx < numWeeks; idx++)
+			{
+				repeatWorkout(w, df.format(cal.getTime()));
+				if(everyWeek)
+					cal.add(Calendar.DATE, 7);
+				else
+					cal.add(Calendar.DATE, 14);
+			}
+		}
+	}
+
 	
 
 	public void changeWorkoutDate(Workout w, String newDateStr)
 	{
-		if(isNewDateValid(newDateStr))
+		if(isDateStrValid(newDateStr))
 		{
 			w.setDate(newDateStr);
 
@@ -37,7 +82,7 @@ public class Schedule implements HttpRequestListener{
 		}
 	}
 
-	private boolean isNewDateValid(String newDateStr) {
+	private boolean isDateStrValid(String newDateStr) {
 		DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
 		Calendar newDateCal = Calendar.getInstance();
 
--- WorkoutBuddyTest/src/com/example/workoutbuddy/test/ScheduleTest.java
+++ WorkoutBuddyTest/src/com/example/workoutbuddy/test/ScheduleTest.java
@@ -4,6 +4,10 @@ import helperClasses.Workout;
 import httpRequests.AsyncHttpPostWrapper;
 import httpRequests.HttpRequestListener;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 
@@ -33,8 +37,11 @@ public class ScheduleTest extends TestCase implements HttpRequestListener{
 	CountDownLatch signal;
 
 	String futureDateStr = "4/03/2054";
+	String pastDateStr = "4/03/2014";
+	String invalidDateStr = "abaca";
 
-
+	Workout w0;
+	
 	/**
 	 * Creates a new signal to signal call to requestComplete
 	 * Creates instance of Wrapper to test AsyncHttpPost
@@ -50,49 +57,120 @@ public class ScheduleTest extends TestCase implements HttpRequestListener{
 		signal = new CountDownLatch(1);
 		wrapper = new AsyncHttpPostWrapper(this);
 	}
-
 	
-	public void testRepeatWorkout_OneTime_ValidDate()
+	
+
+	public void testRepeatWorkout_OneTime_InvalidDateStr()
 	{
-		Workout w1 = getWorkout1();
-		//sch.repeatWorkout(w1, futureDateStr);
-		Assert.assertTrue(false);
+		int numOldWorkouts = getAllWorkouts().length;
+
+		Workout w5 = getWorkout(5);
+		sch.repeatWorkout(w5, this.invalidDateStr);
+		int numNewWorkouts = getNewWorkouts(numOldWorkouts).length;
+		Assert.assertEquals(0, numNewWorkouts);
+		
+		sch.repeatWorkout(w5, this.pastDateStr);
+		numNewWorkouts = getNewWorkouts(numOldWorkouts).length;
+		Assert.assertEquals(0, numNewWorkouts);
+	}
+	
+	
+	
+	public void testRepeatWorkout_OneTime_ValidDateStr()
+	{
+		int numOldWorkouts = getAllWorkouts().length;
+		
+		Workout w5 = getWorkout(5);
+		sch.repeatWorkout(w5, futureDateStr);
+		
+		Workout[] newWorkouts = getNewWorkouts(numOldWorkouts);
+		Assert.assertEquals(1, newWorkouts.length);
+		
+		Assert.assertEquals(w5.getName(), newWorkouts[0].getName());
+		Assert.assertEquals(w5.getDescription(), newWorkouts[0].getDescription());
+		Assert.assertEquals(futureDateStr, newWorkouts[0].getDate());
+	}
+
+	public void testRepeatWorkout_Multiple_EveryWeek() throws ParseException
+	{
+		int numOldWorkouts = getAllWorkouts().length;
+		
+		Workout w5 = getWorkout(5);
+		sch.repeatWorkout(w5, futureDateStr, 5, true);
+
+		DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+		Calendar futureCal = Calendar.getInstance();
+		futureCal.setTime(df.parse(futureDateStr));
+		
+		
+		Workout[] newWorkouts = getNewWorkouts(numOldWorkouts);
+		Assert.assertEquals(5, newWorkouts.length);
+		for(int idx=0; idx<5; idx++)
+		{
+			String dateStr = df.format(futureCal.getTime());
+			Assert.assertEquals(w5.getName(), newWorkouts[idx].getName());
+			Assert.assertEquals(w5.getDescription(), newWorkouts[idx].getDescription());
+			Assert.assertEquals(dateStr, newWorkouts[idx].getDate());
+			futureCal.add(Calendar.DATE, 7);
+		}
+	}
+	
+	public void testRepeatWorkout_Multiple_EveryOtherWeek() throws ParseException
+	{
+		int numOldWorkouts = getAllWorkouts().length;
+		
+		Workout w5 = getWorkout(5);
+		sch.repeatWorkout(w5, futureDateStr, 5, false);
+		
+		DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+		Calendar futureCal = Calendar.getInstance();
+		futureCal.setTime(df.parse(futureDateStr));
+		
+		
+		Workout[] newWorkouts = getNewWorkouts(numOldWorkouts);
+		Assert.assertEquals(5, newWorkouts.length);
+		for(int idx=0; idx<5; idx++)
+		{
+			String dateStr = df.format(futureCal.getTime());
+			Assert.assertEquals(w5.getName(), newWorkouts[idx].getName());
+			Assert.assertEquals(w5.getDescription(), newWorkouts[idx].getDescription());
+			Assert.assertEquals(dateStr, newWorkouts[idx].getDate());
+			futureCal.add(Calendar.DATE, 14);
+		}
 	}
 	
 
 
 	public void testChangeDateWorkout_Future()
 	{
-		Workout w1 = getWorkout1();
+		Workout w1 = getWorkout(1);
 
 		sch.changeWorkoutDate(w1, futureDateStr);
-		w1 = getWorkout1();
+		w1 = getWorkout(1);
 		
 		Assert.assertEquals(futureDateStr, w1.getDate());
 	}
 
 	public void testChangeDateWorkout_Past()
 	{
-		Workout w1 = getWorkout1();
+		Workout w1 = getWorkout(1);
 		String oldDateStr = w1.getDate();
 
-		String pastDateStr = "4/03/2014";
 
 		sch.changeWorkoutDate(w1, pastDateStr);
-		w1 = getWorkout1();
+		w1 = getWorkout(1);
 		
 		Assert.assertEquals(oldDateStr, w1.getDate()); 
 	}
 
 	public void testChangeDateWorkout_Invalid()
 	{
-		Workout w1 = getWorkout1();
+		Workout w1 = getWorkout(1);
 		String oldDateStr = w1.getDate();
 
-		String invalidDateStr = "abaca";
 
 		sch.changeWorkoutDate(w1, invalidDateStr);
-		w1 = getWorkout1();
+		w1 = getWorkout(1);
 		
 		Assert.assertEquals(oldDateStr, w1.getDate()); 
 	}
@@ -104,37 +182,48 @@ public class ScheduleTest extends TestCase implements HttpRequestListener{
 
 	}
 	
-	private Workout getWorkout1()
+	private Workout getWorkout(int wid)
 	{
 		try {
-			return wrapper.getWorkout(1);
+			return wrapper.getWorkout(wid);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return null;
 		} catch (ExecutionException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return null;
 		}
 	}
 	
-	private Workout[] getNewWorkouts()
+
+	private Workout[] getAllWorkouts()
 	{
-		return null;
-//		Workout[] newWorkouts = wrapper.getWorkoutList("usernameA");
-//		
-//		try {
-//			return wrapper.getWorkout(1);
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//			return null;
-//		} catch (ExecutionException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//			return null;
-//		}
-//		
+		Workout[] workoutsAll;
+		
+		try {
+			workoutsAll = wrapper.getWorkoutList("usernameA");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			workoutsAll = new Workout[0];
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+			workoutsAll = new Workout[0];
+		}
+		return workoutsAll;
+	}
+	
+	private Workout[] getNewWorkouts(int numOldWorkouts)
+	{
+		Workout[] workoutsAll = getAllWorkouts();
+		Workout[] workoutsNew;
+		
+		int numNewWorkouts = workoutsAll.length - numOldWorkouts;
+		workoutsNew = new Workout[numNewWorkouts];
+
+		for(int idx = 0; idx < numNewWorkouts; idx++)
+		{
+			workoutsNew[idx] = workoutsAll[numOldWorkouts + idx];
+		}
+		return workoutsNew;
 	}
 }
