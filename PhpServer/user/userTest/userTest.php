fcb42bfd30cb7ebefb8325715f34d795d187d74b
Weixi Liu <mxliuweixi@hotmail.com>
2014-04-10 15:11:02 -0500

    Add parameterized unit test and more secure password registration for user login

--- PhpServer/user/userTest/userTest.php
+++ PhpServer/user/userTest/userTest.php
@@ -4,64 +4,28 @@ require_once '../../helperFunctions.php';
 
 class MySQLConnectTest extends PHPUnit_Framework_TestCase
 {
-    private function addUser($username, $password){
-        //Curl for adding User
-        $url = 'http://workoutbuddy.web.engr.illinois.edu/PhpFiles/addUser.php';
-        $data = array(
-            'username' => $username,
-            'password' => $password);
-        return curlHelper($url, $data);
-    }
-
-    private function login($username, $password){
-        //Curl for login user
-        $getUrl = 'http://workoutbuddy.web.engr.illinois.edu/PhpFiles/userLogin.php';
-        $getData = array(
-            'username' => $username,
-            'password' => $password);
-        return curlHelper($getUrl, $getData);
-    }
-
-    private function deleteUser($username, $password){
-        //Curl for deleting user
-        $deleteUrl = 'http://workoutbuddy.web.engr.illinois.edu/PhpFiles/deleteUser.php';
-        $deleteData = array(
-            'username' => $username,
-            'password' => $password);
-        return curlHelper($deleteUrl, $deleteData);
-    }
-
-    public function testBasic(){
+    /**
+     * @dataProvider provider
+     */
+    public function testLogin($username,$password,$result)
+    {
         $response = $this->addUser('validUser','validPassword');
         $this->assertEquals($response, 'success');
 
-        $getResponse = $this->login('validUser','validPassword');
-        $this->assertEquals($getResponse, 'success');
+        $getResponse = $this->login($username,$password);
+        $this->assertEquals($getResponse, $result);
 
         $deleteResponse = $this->deleteUser('validUser','validPassword');
         $this->assertEquals($deleteResponse, 'successfully deleted');
     }
 
-    public function testInvalidPasswordLogin(){
-        $response = $this->addUser('validUser','validPassword');
-        $this->assertEquals($response, 'success');
-
-        $getResponse = $this->login('validUser','invalidPassword');
-        $this->assertEquals($getResponse, 'fail');
-
-        $deleteResponse = $this->deleteUser('validUser','validPassword');
-        $this->assertEquals($deleteResponse, 'successfully deleted');
-    }
-
-    public function testInvalidUserLogin(){
-        $response = $this->addUser('validUser','validPassword');
-        $this->assertEquals($response, 'success');
-
-        $getResponse = $this->login('invalidUser','invalidPassword');
-        $this->assertEquals($getResponse, 'fail');
-
-        $deleteResponse = $this->deleteUser('validUser','validPassword');
-        $this->assertEquals($deleteResponse, 'successfully deleted');
+    public function provider()
+    {
+        return array(
+            array('validUser','validPassword', 'success'),
+            array('validUser','invalidPassword', 'fail'),
+            array('invalidUser','invalidPassword', 'fail')
+        );
     }
 
     public function testInvalidUserAdd(){
@@ -89,8 +53,34 @@ class MySQLConnectTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($deleteResponse, 'successfully deleted');
     }
 
+    private function addUser($username, $password){
+        //Curl for adding User
+        $url = 'http://workoutbuddy.web.engr.illinois.edu/PhpFiles/addUser.php';
+        $data = array(
+            'username' => $username,
+            'password' => $password);
+        return curlHelper($url, $data);
+    }
+
+    private function login($username, $password){
+        //Curl for login user
+        $getUrl = 'http://workoutbuddy.web.engr.illinois.edu/PhpFiles/userLogin.php';
+        $getData = array(
+            'username' => $username,
+            'password' => $password);
+        return curlHelper($getUrl, $getData);
+    }
+
+    private function deleteUser($username, $password){
+        //Curl for deleting user
+        $deleteUrl = 'http://workoutbuddy.web.engr.illinois.edu/PhpFiles/deleteUser.php';
+        $deleteData = array(
+            'username' => $username,
+            'password' => $password);
+        return curlHelper($deleteUrl, $deleteData);
+    }
+
 
 }
-
 
 ?>
