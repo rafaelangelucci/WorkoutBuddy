a163c976b6398a5f4f122f89b41b886cff7e5953
Weixi Liu <mxliuweixi@hotmail.com>
2014-04-10 13:54:27 -0500

    Secure user login with hash

--- PhpServer/user/addUser.php
+++ PhpServer/user/addUser.php
@@ -1,12 +1,14 @@
 <?php
     // Include Medoo
-    require_once 'medoo.php';
+    require_once '../medoo.php';
     // Initialize
     $database = new medoo('workoutbuddy_01');
+    $salt = "uiucworkoutbuddysalt";
 
     if(isset($_POST['username'], $_POST['password'])){
         $username = $_POST['username'];
         $password = $_POST['password'];
+        $password = hash('sha512', $password.$salt);
         $data = array('username','password');
         $where = array('username' => $username);
         $response = $database->select('User', $data, $where);
--- PhpServer/user/deleteUser.php
+++ PhpServer/user/deleteUser.php
@@ -1,13 +1,15 @@
 <?php
 
     // Include Medoo
-    require_once 'medoo.php';
+    require_once '../medoo.php';
     // Initialize
     $database = new medoo('workoutbuddy_01');
+    $salt = "uiucworkoutbuddysalt";
 
     if(isset($_POST['username'], $_POST['password'])){
         $username = $_POST['username'];
         $password = $_POST['password'];
+        $password = hash('sha512', $password.$salt);
         $data = array('username');
         $where = array('username' => $username);
         $response = $database->select('User', $data, $where);
