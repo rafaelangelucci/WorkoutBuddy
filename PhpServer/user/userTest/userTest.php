16604133cb8658ae8d2231679096a92bf2136860
Weixi Liu <mxliuweixi@hotmail.com>
2014-04-25 01:39:44 -0500

    Refactor tests for User login

--- PhpServer/user/userTest/userTest.php
+++ PhpServer/user/userTest/userTest.php
@@ -7,12 +7,16 @@ class MySQLConnectTest extends PHPUnit_Framework_TestCase
     /**
      * @dataProvider provider
      */
-    public function testLogin($username,$password,$result)
+    public function testLogin($username,$password,$login,$result)
     {
         $response = $this->addUser('validUser','validPassword');
         $this->assertEquals($response, 'success');
 
-        $getResponse = $this->login($username,$password);
+        if($login){
+            $getResponse = $this->login($username,$password);
+        }else{
+            $getResponse = $this->addUser($username,$password);
+        }
         $this->assertEquals($getResponse, $result);
 
         $deleteResponse = $this->deleteUser('validUser','validPassword');
@@ -22,21 +26,11 @@ class MySQLConnectTest extends PHPUnit_Framework_TestCase
     public function provider()
     {
         return array(
-            array('validUser','validPassword', 'success'),
-            array('validUser','invalidPassword', 'fail'),
-            array('invalidUser','invalidPassword', 'fail')
+            array('validUser','validPassword',true, 'success'),
+            array('validUser','invalidPassword',true, 'fail'),
+            array('invalidUser','invalidPassword',true, 'fail'),
+            array('validUser','validPassword',false, 'username already exists')
         );
-    }
-
-    public function testInvalidUserAdd(){
-        $response = $this->addUser('validUser','validPassword');
-        $this->assertEquals($response, 'success');
-
-        $response = $this->addUser('validUser','validPassword');
-        $this->assertEquals($response, 'username already exists');
-
-        $deleteResponse = $this->deleteUser('validUser','validPassword');
-        $this->assertEquals($deleteResponse, 'successfully deleted');
     }
 
     public function testDeleteUser(){
@@ -55,29 +49,28 @@ class MySQLConnectTest extends PHPUnit_Framework_TestCase
 
     private function addUser($username, $password){
         //Curl for adding User
-        $url = 'http://workoutbuddy.web.engr.illinois.edu/PhpFiles/addUser.php';
-        $data = array(
-            'username' => $username,
-            'password' => $password);
-        return curlHelper($url, $data);
+        $file = 'addUser.php';
+        return $this->urlConnector($username, $password, $file);
     }
 
     private function login($username, $password){
         //Curl for login user
-        $getUrl = 'http://workoutbuddy.web.engr.illinois.edu/PhpFiles/userLogin.php';
-        $getData = array(
-            'username' => $username,
-            'password' => $password);
-        return curlHelper($getUrl, $getData);
+        $file = 'userLogin.php';
+        return $this->urlConnector($username, $password, $file);
     }
 
     private function deleteUser($username, $password){
         //Curl for deleting user
-        $deleteUrl = 'http://workoutbuddy.web.engr.illinois.edu/PhpFiles/deleteUser.php';
-        $deleteData = array(
+        $file = 'deleteUser.php';
+        return $this->urlConnector($username, $password, $file);
+    }
+
+    private function urlConnector($username, $password, $file)
+    {
+        $data = array(
             'username' => $username,
             'password' => $password);
-        return curlHelper($deleteUrl, $deleteData);
+        return curlHelper('http://workoutbuddy.web.engr.illinois.edu/PhpFiles/'.$file, $data);
     }
 
 
