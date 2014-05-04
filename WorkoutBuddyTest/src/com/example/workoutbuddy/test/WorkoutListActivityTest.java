a8dc56c10b9ef2557a9d8d2340209b1a03105182
tmadigan7 <tmadigan7@hotmail.com>
2014-05-04 16:26:08 -0500

    final,madigan6: Refactored - extracted method: cleaning WorkoutListActivityTest

--- WorkoutBuddyTest/src/com/example/workoutbuddy/test/WorkoutListActivityTest.java
+++ WorkoutBuddyTest/src/com/example/workoutbuddy/test/WorkoutListActivityTest.java
@@ -4,33 +4,19 @@ import helperClasses.Exercise;
 import helperClasses.Workout;
 
 import java.util.ArrayList;
-
-import junit.framework.Assert;
-import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.Intent;
-import android.test.ActivityInstrumentationTestCase2;
 import android.test.ActivityUnitTestCase;
-import android.test.UiThreadTest;
-import android.test.ViewAsserts;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
-
 import com.uiuc.workoutbuddy.UseWorkoutActivity;
 import com.uiuc.workoutbuddy.WorkoutListActivity;
-
 import customListAdapter.WorkoutListAdapter;
 
-//public class WorkoutListActivityTest extends ActivityInstrumentationTestCase2<WorkoutListActivity> {
 public class WorkoutListActivityTest extends ActivityUnitTestCase<WorkoutListActivity> {
 
 	private WorkoutListActivity activity;
-	private TextView name;
-	private TextView desc;
 	private ListView list;
-
+	private WorkoutListAdapter adapter;
 
 	public WorkoutListActivityTest(){
 		super(WorkoutListActivity.class);
@@ -49,6 +35,10 @@ public class WorkoutListActivityTest extends ActivityUnitTestCase<WorkoutListAct
 		super.tearDown();
 	}
 
+	/**
+	 * Function that generates mock workout objects to inject into list
+	 * @return ArrayList of workout objects
+	 */
 	public ArrayList<Workout> mockWorkouts()
 	{
 		ArrayList<Workout> workouts = new ArrayList<Workout>();
@@ -60,50 +50,60 @@ public class WorkoutListActivityTest extends ActivityUnitTestCase<WorkoutListAct
 		return workouts;
 	}
 
+	/**
+	 * Test activity created successfully
+	 */
 	public void testViewsCreated() {
 		assertNotNull(getActivity());
 	}
 
+	/**
+	 * Test correct activity was launched
+	 */
 	public void testActivityStartup()
 	{
 		final String expected = activity.getString(com.uiuc.workoutbuddy.R.string.app_name);
 		assertEquals(expected, "Workout\nBuddy");
 	}
 
-	public final void testItemClick() {
+	/**
+	 * Test to check proper list creation with specified size
+	 */
+	public final void testListCreation() {
 		getInstrumentation().callActivityOnStart(activity);
 		getInstrumentation().callActivityOnResume(activity);
 
 		// Check if list exists
-		ListView list = activity.getListView();
+		list = activity.getListView();
 		assertNotNull("Intent was null", list);
 
 		// Load test data
-		WorkoutListAdapter adapter = new WorkoutListAdapter(getInstrumentation().getContext(), mockWorkouts());
+		adapter = new WorkoutListAdapter(getInstrumentation().getContext(), mockWorkouts());
 		list.setAdapter(adapter);
 		adapter.notifyDataSetChanged();
 
 		assertTrue(adapter.getCount() > 0);
 		assertTrue(adapter.getCount() == 3);
-
-		/*
-		// Check if list has at least one item to click
-		View firstItem = list.getAdapter().getView(0, null, null);
-		assertNotNull(firstItem);
-
-		// Perform a click on the first item
-		list.performItemClick(
-				firstItem,
-				0,
-				list.getAdapter().getItemId(0)
-				);
-
-		// Check if the contact details activity got started
-		Intent intent = getStartedActivityIntent();
-		assertNotNull(intent);
-		assertEquals(
-				ContactDetailActivity.class.getName(),
-				intent.getComponent().getClassName()
-				);*/
+	}
+	
+	/**
+	 * Test to execute list item click and corresponding activity startup
+	 */
+	public final void testItemClick()
+	{
+		if(list != null && adapter != null)
+		{
+			// Check if list has at least one item to click
+			View firstItem = list.getAdapter().getView(0, null, null);
+			assertNotNull(firstItem);
+	
+			// Perform a click on the first item
+			list.performItemClick(firstItem, 0, list.getAdapter().getItemId(0));
+	
+			// Check if the contact details activity got started
+			Intent intent = getStartedActivityIntent();
+			assertNotNull(intent);
+			assertEquals(UseWorkoutActivity.class.getName(),intent.getComponent().getClassName());
+		}
 	}
 }
