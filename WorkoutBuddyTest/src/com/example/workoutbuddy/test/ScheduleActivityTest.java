ce3aaf1df5da6bb8a3549cdfedcba35169f24fe6
meyer30 <meyer30@illinois.edu>
2014-05-04 20:36:59 -0500

    Final,meyer30 Commented and got rid of old tests

--- WorkoutBuddyTest/src/com/example/workoutbuddy/test/ScheduleActivityTest.java
+++ WorkoutBuddyTest/src/com/example/workoutbuddy/test/ScheduleActivityTest.java
@@ -1,20 +1,17 @@
 package com.example.workoutbuddy.test;
 
-import helperClasses.Exercise;
 import helperClasses.Workout;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-
-
 import junit.framework.Assert;
-
 import com.uiuc.workoutbuddy.ScheduleActivity;
-
 import android.test.ActivityInstrumentationTestCase2;
 import android.view.View;
+import android.widget.CheckBox;
 
+/**
+ * This class is for Testing ScheduleActivity
+ * @author Joel
+ *
+ */
 public class ScheduleActivityTest extends
 ActivityInstrumentationTestCase2<ScheduleActivity> {
 
@@ -24,19 +21,13 @@ ActivityInstrumentationTestCase2<ScheduleActivity> {
 	int numbetween;
 	Workout w;
 
-	public ScheduleActivityTest(Class<ScheduleActivity> activityClass) {
-		super(activityClass);
+	/**
+	 * Basic Constructor
+	 */
+	public ScheduleActivityTest() {
+		super(ScheduleActivity.class);
 	}
 
-//
-//	public ScheduleActivityTest(Class<ScheduleActivity> activityClass, Workout w, String dateStr, int numTimes, int numBetween) {
-//		super(activityClass);
-//
-//		date = dateStr;
-//		numtimes = numTimes;
-//		numbetween = numBetween;
-//		this.w = w;
-//	}
 
 	@Override
 	protected void setUp() throws Exception {
@@ -48,7 +39,10 @@ ActivityInstrumentationTestCase2<ScheduleActivity> {
 	}
 
 
-	public void testLayoutVisible(){
+	/**
+	 * Tests what should be visible at the beginning of the activity.
+	 */
+	public void testLayoutVisibleBegining(){
 		View v;
 		v = myActivity.findViewById(com.uiuc.workoutbuddy.R.id.spnr_workouts);
 		Assert.assertTrue(v.isShown());
@@ -57,37 +51,12 @@ ActivityInstrumentationTestCase2<ScheduleActivity> {
 		v = myActivity.findViewById(com.uiuc.workoutbuddy.R.id.cb_repeat);
 		Assert.assertTrue(v.isShown());
 		v = myActivity.findViewById(com.uiuc.workoutbuddy.R.id.et_numbetween);
-		Assert.assertTrue(v.isShown());
+		Assert.assertFalse(v.isShown());
 		v = myActivity.findViewById(com.uiuc.workoutbuddy.R.id.et_numtimes);
-		Assert.assertTrue(v.isShown());
+		Assert.assertFalse(v.isShown());
 		v = myActivity.findViewById(com.uiuc.workoutbuddy.R.id.btn_done);
 		Assert.assertTrue(v.isShown());
 		v = myActivity.findViewById(com.uiuc.workoutbuddy.R.id.btn_cancel);
 		Assert.assertTrue(v.isShown());
 	}
-
-
-//
-//	@Parameters
-//	public static Collection<Object[]> generateData()
-//	{   
-//		Workout("TestWorkout", "4/03/2054", null, "usernameA"String desc, String username,
-//				ArrayList<Exercise> exercises)
-//		Workout w = new Workout()
-//		ArrayList<Object[]> params = new ArrayList<Object[]>();
-//		params.add(	new Object[]{ new ScheduleActivity(), "4/03/2054", 5, 5 });
-//		params.add(	new Object[]{ new ScheduleActivity(), "4/03/2054", 5, 5 });
-//		params.add(	new Object[]{ new ScheduleActivity(), "4/03/2054", 5, 5 });
-//		params.add(	new Object[]{ new ScheduleActivity(), "4/03/2054", 5, 5 });
-//		params.add(	new Object[]{ new ScheduleActivity(), "4/03/2054", 5, 5 });
-//		return params;
-//	}
-//
-//
-//
-//	@Test
-//	public void testRepeatWorkout()
-//	{
-//		Assert.assertTrue(myActivity.repeatWorkout(this.w, this.date, this.numtimes, this.numbetween);
-//	}
 }
