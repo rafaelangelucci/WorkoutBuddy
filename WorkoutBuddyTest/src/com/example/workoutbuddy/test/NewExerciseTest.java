acf1775001a078ff1f2d44aa95a781033931d1d0
dwilson@illinois.edu <dwilson@illinois.edu>
2014-05-04 23:19:38 -0500

    Ctrl-Shift-F

--- WorkoutBuddyTest/src/com/example/workoutbuddy/test/NewExerciseTest.java
+++ WorkoutBuddyTest/src/com/example/workoutbuddy/test/NewExerciseTest.java
@@ -16,9 +16,12 @@ import android.widget.Spinner;
 
 /**
  * Tests for the NewExerciseActivity class.
+ * 
  * @author Daniel
  */
-public class NewExerciseTest extends ActivityInstrumentationTestCase2<NewExerciseActivity> implements HttpRequestListener {
+public class NewExerciseTest extends
+		ActivityInstrumentationTestCase2<NewExerciseActivity> implements
+		HttpRequestListener {
 	NewExerciseActivity mActivity;
 	Instrumentation mInstrumentation;
 	EditText mTextName;
@@ -29,102 +32,136 @@ public class NewExerciseTest extends ActivityInstrumentationTestCase2<NewExercis
 	public NewExerciseTest() {
 		super(NewExerciseActivity.class);
 	}
-	
+
 	protected void setUp() throws Exception {
 		super.setUp();
 
-        mInstrumentation = getInstrumentation();
-	    setActivityInitialTouchMode(false);
-        mActivity = getActivity();
-        mTextName = (EditText) mActivity.findViewById(com.uiuc.workoutbuddy.R.id.edit_exercise_name);
-        mTextDesc = (EditText) mActivity.findViewById(com.uiuc.workoutbuddy.R.id.edit_exercise_description);
-        mSpinType = (Spinner) mActivity.findViewById(com.uiuc.workoutbuddy.R.id.spinner_exercise_type);
-        mButton = (Button) mActivity.findViewById(com.uiuc.workoutbuddy.R.id.btn_new_exercise_ok);
+		mInstrumentation = getInstrumentation();
+		setActivityInitialTouchMode(false);
+		mActivity = getActivity();
+		mTextName = (EditText) mActivity
+				.findViewById(com.uiuc.workoutbuddy.R.id.edit_exercise_name);
+		mTextDesc = (EditText) mActivity
+				.findViewById(com.uiuc.workoutbuddy.R.id.edit_exercise_description);
+		mSpinType = (Spinner) mActivity
+				.findViewById(com.uiuc.workoutbuddy.R.id.spinner_exercise_type);
+		mButton = (Button) mActivity
+				.findViewById(com.uiuc.workoutbuddy.R.id.btn_new_exercise_ok);
 	}
-	
+
 	/**
-	 * Test whether the activity is able to run without crashing 
+	 * Test whether the activity is able to run without crashing
 	 */
 	public void testStaysOpen() {
-        assertFalse(getActivity().isFinishing());
+		assertFalse(getActivity().isFinishing());
 	}
-	
+
 	/**
 	 * Test that the program will not accept an exercise with no name.
+	 * 
 	 * @throws Throwable
 	 */
 	public void testNeedName() throws Throwable {
-		runTestOnUiThread(new Runnable() {public void run() {
-	 		mButton.performClick();
-	    }});
-        assertFalse(getActivity().isFinishing());
+		runTestOnUiThread(new Runnable() {
+			public void run() {
+				mButton.performClick();
+			}
+		});
+		assertFalse(getActivity().isFinishing());
 	}
-	
+
 	/**
 	 * Test that the spinner is able to accept and read value correctly.
+	 * 
 	 * @throws Throwable
 	 */
 	public void testSpinnerValues() throws Throwable {
 		assertEquals("Reps", mSpinType.getSelectedItem().toString());
-		runTestOnUiThread(new Runnable() {public void run() {mSpinType.setSelection(2);}});
+		runTestOnUiThread(new Runnable() {
+			public void run() {
+				mSpinType.setSelection(2);
+			}
+		});
 		assertEquals("Time", mSpinType.getSelectedItem().toString());
-		runTestOnUiThread(new Runnable() {public void run() {mSpinType.setSelection(1);}});
+		runTestOnUiThread(new Runnable() {
+			public void run() {
+				mSpinType.setSelection(1);
+			}
+		});
 		assertEquals("Weight", mSpinType.getSelectedItem().toString());
-		runTestOnUiThread(new Runnable() {public void run() {mSpinType.setSelection(0);}});
+		runTestOnUiThread(new Runnable() {
+			public void run() {
+				mSpinType.setSelection(0);
+			}
+		});
 		assertEquals("Reps", mSpinType.getSelectedItem().toString());
 	}
-	
+
 	/**
 	 * Test that the name box is set up correctly and accepts text.
+	 * 
 	 * @throws Throwable
 	 */
 	public void testNameBox() throws Throwable {
-		assertEquals("",mTextName.getText().toString());
-		runTestOnUiThread(new Runnable() {public void run() {
-			mTextName.setText("Unit Test Name");
-		}});
+		assertEquals("", mTextName.getText().toString());
+		runTestOnUiThread(new Runnable() {
+			public void run() {
+				mTextName.setText("Unit Test Name");
+			}
+		});
 		assertEquals("Unit Test Name", mTextName.getText().toString());
 	}
-	
+
 	/**
 	 * Test that the description box is set up correctly and accepts text.
+	 * 
 	 * @throws Throwable
 	 */
 	public void testDescBox() throws Throwable {
-		assertEquals("",mTextDesc.getText().toString());
-		runTestOnUiThread(new Runnable() {public void run() {
-			mTextDesc.setText("Testing the description box...");
-		}});
-		assertEquals("Testing the description box...", mTextDesc.getText().toString());
+		assertEquals("", mTextDesc.getText().toString());
+		runTestOnUiThread(new Runnable() {
+			public void run() {
+				mTextDesc.setText("Testing the description box...");
+			}
+		});
+		assertEquals("Testing the description box...", mTextDesc.getText()
+				.toString());
 	}
-	
+
 	/**
-	 * Test that an exercise is actually created with the entered information when everything is done correctly.
+	 * Test that an exercise is actually created with the entered information
+	 * when everything is done correctly.
+	 * 
 	 * @throws Throwable
 	 */
 	public void testCreation() throws Throwable {
-		//Enter the information
-        LoginActivity.userName="usernameA";
-		runTestOnUiThread(new Runnable() {public void run() {
-			mTextName.setText("Unit Test Exercise");
-			mTextDesc.setText("This exercise was created for unit testing and should have been deleted immediately.");
-			mSpinType.setSelection(2);
-			
-			//press OK
-	 		mButton.performClick();
-	    }});
-        assertTrue(getActivity().isFinishing());
-        
-        //Read the most recent exercise and confirm it has the correct values.
-        AsyncHttpPostWrapper wrapper = new AsyncHttpPostWrapper(this);
-        Exercise[] exercises = wrapper.getExerciseList(LoginActivity.userName);
-        Exercise createdExercise = exercises[exercises.length-1];
-        assertEquals("Unit Test Exercise", createdExercise.getName());
-        assertEquals("This exercise was created for unit testing and should have been deleted immediately.", createdExercise.getDescription());
-        assertEquals("Time", createdExercise.getType());
-        
-        //delete the created exercise for cleanliness.
-        wrapper.deleteExercise(createdExercise.getEid());
+		// Enter the information
+		LoginActivity.userName = "usernameA";
+		runTestOnUiThread(new Runnable() {
+			public void run() {
+				mTextName.setText("Unit Test Exercise");
+				mTextDesc
+						.setText("This exercise was created for unit testing and should have been deleted immediately.");
+				mSpinType.setSelection(2);
+
+				// press OK
+				mButton.performClick();
+			}
+		});
+		assertTrue(getActivity().isFinishing());
+
+		// Read the most recent exercise and confirm it has the correct values.
+		AsyncHttpPostWrapper wrapper = new AsyncHttpPostWrapper(this);
+		Exercise[] exercises = wrapper.getExerciseList(LoginActivity.userName);
+		Exercise createdExercise = exercises[exercises.length - 1];
+		assertEquals("Unit Test Exercise", createdExercise.getName());
+		assertEquals(
+				"This exercise was created for unit testing and should have been deleted immediately.",
+				createdExercise.getDescription());
+		assertEquals("Time", createdExercise.getType());
+
+		// delete the created exercise for cleanliness.
+		wrapper.deleteExercise(createdExercise.getEid());
 	}
 
 	@Override
